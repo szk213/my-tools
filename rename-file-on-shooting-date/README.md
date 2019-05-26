24d710eb706b2a1f39cf13898cc0c1988e032ce9
szk213 <szk@szk213.com>
2019-05-26 18:24:22 +0900

    ファイル名を撮影日に変換

---
+++ rename-file-on-shooting-date/README.md
@@ -0,0 +1,9 @@
+# rename-file-on-shooting-date
+
+写真のファイル名を撮影日にリネーム(jpg だけ)
+
+## 使い方
+
+config.jsonに変換前,変換後のファイルを指定
+
+`node main.js`
---
+++ rename-file-on-shooting-date/main.js
@@ -0,0 +1,51 @@
+const sharp = require("sharp");
+const exif = require("exif-reader");
+const path = require("path");
+const fs = require("fs").promises;
+const moment = require("moment");
+const config = require("./config.json");
+
+function getShootingDate(filePath) {
+  return new Promise(resolve => {
+    sharp(filePath)
+      .metadata()
+      .then(function(metadata) {
+        if (exif(metadata.exif).exif) {
+          resolve(exif(metadata.exif).exif.DateTimeOriginal);
+        }
+        resolve();
+      });
+  });
+}
+
+async function getFiles(targetPath) {
+  const dirents = await fs.readdir(targetPath, { withFileTypes: true });
+  const files = dirents
+    .filter(dirent => dirent.isFile() && /.*\.jpg$/.test(dirent.name))
+    .map(dirent => path.join(targetPath, dirent.name));
+  return files;
+}
+
+function getNewFileName(shootingDate) {
+  return `${moment(shootingDate).format("YYYYMMDD_HHmmss")}.jpg`;
+}
+
+async function main() {
+  const target = config.targetDir;
+  const output = config.outputDir;
+
+  await fs.mkdir(output, { recursive: true });
+
+  const files = await getFiles(target);
+  Promise.all(
+    files.map(async filePath => {
+      const shootingDate = await getShootingDate(filePath);
+      return fs.rename(
+        filePath,
+        path.join(output, getNewFileName(shootingDate))
+      );
+    })
+  );
+}
+
+main();
